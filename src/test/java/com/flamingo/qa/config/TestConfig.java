f3c4c535034114643ee29cf17ea765f8403c1147
dmitrygrishanin <dmitrygrishanin@gmail.com>
2026-05-13 12:14:59 +0300

    TestConfig file is added

---
+++ src/test/java/com/flamingo/qa/config/TestConfig.java
@@ -0,0 +1,15 @@
+package com.flamingo.qa.config;
+
+public final class TestConfig {
+    public static final String RESTFUL_BOOKER_BASE_URL = "https://restful-booker.herokuapp.com";
+
+    public static final String BOOKER_USERNAME = "admin";
+    public static final String BOOKER_PASSWORD = "password123";
+
+    public static final String DEMOQA_BASE_URL = "https://demoqa.com";
+    public static final String PRACTICE_FORM_URL = DEMOQA_BASE_URL + "/automation-practice-form";
+    public static final String WEB_TABLES_URL = DEMOQA_BASE_URL + "/webtables";
+
+    private TestConfig() {
+    }
+}
