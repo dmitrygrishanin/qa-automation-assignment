7ee623f510676b0f48ebcc03999c76b2390e0f25
dmitrygrishanin <dmitrygrishanin@gmail.com>
2026-05-14 10:21:21 +0300

    HYGRAPH_GRAPHQL_BASE_URL is added

--- src/test/java/com/flamingo/qa/config/TestConfig.java
+++ src/test/java/com/flamingo/qa/config/TestConfig.java
@@ -2,6 +2,8 @@ package com.flamingo.qa.config;
 
 public final class TestConfig {
     public static final String RESTFUL_BOOKER_BASE_URL = "https://restful-booker.herokuapp.com";
+    public static final String HYGRAPH_GRAPHQL_BASE_URL =
+            "https://eu-central-1-shared-euc1-02.cdn.hygraph.com/content/clv3hcefv000001w6a4f3at2l/master";
 
     public static final String BOOKER_USERNAME = "admin";
     public static final String BOOKER_PASSWORD = "password123";
